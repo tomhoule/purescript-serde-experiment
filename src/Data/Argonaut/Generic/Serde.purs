119d00f3b6073d8d06b531ad4769880151c8b320
Tom Houlé <tom@tomhoule.com>
2017-07-30 16:18:11 +0200

    wip

--- src/Data/Argonaut/Generic/Serde.purs
+++ src/Data/Argonaut/Generic/Serde.purs
@@ -39,7 +39,7 @@ options = Options {
 , userEncoding             : userEncoding
 , userDecoding             : userDecoding
 , fieldLabelModifier       : id
-, omitNothingFields        : false
+, omitNothingFields        : true
 }
 
 sumEncoding :: SumEncoding
